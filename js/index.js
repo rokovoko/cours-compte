2539da59f0124bb00f908c2f16ab1b549ec460c2
saraguerric <sara.guerric@gmail.com>
2017-01-21 12:32:41 +0100

    Add: finitions

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-var operateur=[], secteur=[], entreprise=[], keys={}, width=80, state=0;
+var operateur=[], secteur=[], entreprise=[], keys={}, width=80, state=0, prevState=0;
 
 function stringify (str) {
   return str.toLowerCase().replace(/ /g, '-').replace(/é|è/g, 'e').replace(/ô/g, 'o');
 }
 
+
+// ------------ SELECTS -------------//
+// ------------ SELECTS -------------//
+// ------------ SELECTS -------------//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 function lock (item, g) {
   $('.' + g + ' .slot').fadeOut();
   $('.' + g + ' .slot-select').fadeIn();
@@ -25,7 +34,8 @@ function select (el) {
   var i = parseInt($(el).parent().parent().attr('index'));
   var g = $(el).parent().parent().attr('group');
   var tab = window[g];
-
+  prevState = state;
+  changeBPI(g);
   if ( g != 'entreprise') {
     $('.wrapper:not(.' + g + ')').attr('index', 0);
     $('.wrapper.secteur:not(.' + g + ')').find('.title').html(keys[secteur[0]].title);
@@ -42,9 +52,13 @@ function select (el) {
 
     $('li').removeClass('hide');
     $('li').addClass('hide');
-    keys[tab[i]].attr.split(' ').forEach(function (a){
-      $('.wrapper:not(' + g + ') .' + a).removeClass('hide');
-    });
+
+    if (g == 'secteur') {
+      keys[tab[i]].attr.split(' ').forEach(function (a){
+        $('.wrapper:not(.operateur):not(.entreprise) .' + a).removeClass('hide');
+      });
+    }
+    $('.wrapper:not(' + g + ') .' + keys[tab[i]].self).removeClass('hide');
     $('.wrapper.operateur .num').html( $('.wrapper.operateur li:not(.hide)').length );
     $('.wrapper.secteur .num').html( $('.wrapper.secteur li:not(.hide)').length );
     $('.wrapper.entreprise .num').html( $('.wrapper.entreprise li:not(.hide)').length );
@@ -61,27 +75,94 @@ function select (el) {
   }
 }
 
-function changeSlide (el, direction) {
-  var i = parseInt($(el).parent().parent().attr('index'));
-  var tab = window[$(el).parent().parent().attr('group')];
-  $(el).parent().find('.index' + i).removeClass('current');
-  i += direction;
+$('footer .back').click( function () {
+  $('li').removeClass('hide');
+  $('.wrapper.operateur .num').html( $('.wrapper.operateur li').length );
+  $('.wrapper.secteur .num').html( $('.wrapper.secteur li').length );
+  $('.wrapper.entreprise .num').html( $('.wrapper.entreprise li').length );
+  if ( state == 2 ) {
+    $('.selected').fadeOut();
+    $('footer .date').fadeOut();
+    $('.left-fiche div.legend').fadeOut();
+    if (prevState == 0) {
+      $('footer .back').fadeOut();
+    }
+  }
+  else {
+    $('.slot').fadeIn();
+    $('.slot-select').fadeOut();
+  }
+  state = prevState;
+});
 
-  if (i >= tab.length) {
-    i = 0;
+// ------------ SLIDER -------------//
+// ------------ SLIDER -------------//
+// ------------ SLIDER -------------//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+function changeBPI (g) {
+  if ( g == 'entreprise' ) {
+    keys['bpifrance'] = {
+      "self" : "bpifrance",
+      "attr": "ape caisse-des-depots services-et-finance",
+      "title" : "Bpifrance",
+      "operateur" : "APE et Caisse des Dépôts",
+      "secteur" : "Services et Finance",
+      "chiffre" : "1,331",
+      "capital" : "50.00"
+    }
   }
-  else if (i < 0) {
-    i = tab.length - 1;
+  else if ( g == 'operateur') {
+    keys['bpifrance'] = {
+      attr: "defense-et-aeronautique energie-et-matieres-premieres services-et-finance industrie-et-telecoms",
+      self: "bpifrance",
+      title: "Bpifrance"
+    }
   }
-  $(el).parent().find('.index' + i).addClass('current');
-  $(el).parent().parent().attr('index', i);
-  $(el).parent().find('ul').stop().animate({'left': i * -width + 'px'}, 600);
-  $(el).parent().find('.title').html(keys[tab[i]].title);
+}
+
+function changeSlide (el, direction) {
+  var g = $(el).parent().parent().attr('group');
+  var tab = window[g];
+  var old = '.wrapper.' + g + ' .current';
+  var oldIndex = parseInt($(old).attr('index'));
+  console.log('this is: ',$(old).prev('li:not(.hide)'))
+  if ( direction < 0 ) {
+    if ($(old).prev('li:not(.hide)')) {
+      var newIndex = parseInt($(old).prev('li:not(.hide)').attr('index'));
+    }
+    else {
+      var newIndex = parseInt($('.wrapper.' + g + ' li:not(.hide)').length-1);
+    }
+  }
+  else {
+    if ($(old).next('li:not(.hide)')) {
+      var newIndex = parseInt($(old).next('li:not(.hide)').attr('index'));
+    }
+    else {
+      var newIndex = 0;
+    }
+  }
+  console.log( 'a : ' , $(newIndex))
+  console.log( 'b : ' , $('.wrapper.' + g + ' .index' + newIndex).attr('self'))
+  console.log( 'c : ' , keys[$('.wrapper.' + g + ' .index' + newIndex).attr('self')])
+
+  $('.wrapper.' + g + ' .index' + oldIndex).removeClass('current');
+  $('.wrapper.' + g + ' .index' + newIndex).addClass('current');
+  $('.wrapper.' + g).attr('index', newIndex);
+  $('.wrapper.' + g).find('ul').stop().animate({'left': newIndex * -width + 'px'}, 600);
+  $('.wrapper.' + g).find('.title').html(keys[$('.wrapper.' + g + ' .index' + newIndex).attr('self')].title);
 }
 
 function Slider (tab, group) {
+  changeBPI(group);
+  var sliderName = group == 'operateur' ? 'opérateur' : group;
   var innerHTML = '<div class="' + group + ' wrapper">'
-      innerHTML+= '<div class="slot"><div class="group">' + group.toUpperCase() + ' :</div>'
+      innerHTML+= '<div class="slot"><div class="group ghost-center"><p>' + sliderName.toUpperCase() + ' :</p></div>'
       innerHTML+= '<div class="num">' + tab.length + '</div>'
       innerHTML+= '<div class="arrow"></div>'
       innerHTML+= '<div class="prev"><i class="fa fa-chevron-circle-left" aria-hidden="true"></i></div>'
@@ -101,7 +182,7 @@ function Slider (tab, group) {
     var c = i == 0 ? keys[each].attr + ' current' : keys[each].attr;
     var self = keys[each].self
     var src = 'https://rokovoko.github.io/cours-compte/img/' + this.group + 's/' + keys[each].self + '.png';
-    this.rail.append('<li class="' + c + ' index' + i + ' ' + self + '" self="' + self + '"><img src="' + src + '" alt="' + self + '"/></li>');
+    this.rail.append('<li class="' + c + ' index' + i + ' ' + self + '" index="' + i + '" self="' + self + '"><img src="' + src + '" alt="' + self + '"/></li>');
   }.bind(this));
 
   $('.' + group + '.wrapper .prev').click(function () {
@@ -117,6 +198,15 @@ function Slider (tab, group) {
   });
 }
 
+
+// ------------ DATA -------------//
+// ------------ DATA -------------//
+// ------------ DATA -------------//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 $.get('data/data.json').then( function (data) {
 
   data.rows.forEach(function ( firm ){
@@ -146,41 +236,27 @@ $.get('data/data.json').then( function (data) {
     keys[f] = firm;
   });
 
-  keys['bpifrance'] = {
-    "self" : "bpifrance",
-    "attr": "ape caisse-des-depots services-et-finance",
-    "title" : "Bpifrance",
-    "operateur" : "APE et Caisse des Dépôts",
-    "secteur" : "Services et Finance",
-    "chiffre" : "1,331",
-    "capital" : "50.00"
-  };
-
   var oSlider = new Slider(operateur, 'operateur');
   var sSlider = new Slider(secteur, 'secteur');
   var fSlider = new Slider(entreprise, 'entreprise');
   console.log(keys);
 });
 
+// ------------ PIE CHARTS -------------//
+// ------------ PIE CHARTS -------------//
+// ------------ PIE CHARTS -------------//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 function afterDraw (pie, total) {
   clearInterval(pie);
   var stringTotal = total.replace(/\./,',');
   stringTotal = stringTotal.replace(/,00/,'');
-  $('.left-fiche div.legend').html(stringTotal).fadeIn(600);
+  $('.left-fiche div.legend').html(stringTotal).fadeIn();
 }
-
-$('footer .back').click( function () {
-  if ( state == 2 ) {
-    $('.selected').fadeOut(600);
-    $('footer .date').fadeOut();
-  }
-  else {
-    $('.slot').fadeIn();
-    $('.slot-select').fadeOut();
-    $('footer .back').fadeOut();
-  }
-});
-
 
 function drawPie (total) {
 
@@ -192,17 +268,17 @@ function drawPie (total) {
   function chart () {
     var slice = Math.PI*2*(percent/100);
     ctx.clearRect(0, 0, px*2, px*2);
-    ctx.fillStyle = '#26B4D5';
+    ctx.fillStyle = '#D2DDF0';
   	ctx.beginPath();
   	ctx.moveTo(px,px);
-  	ctx.arc(px,px,px, 0,slice, false);
+  	ctx.arc(px,px,px, 0,slice, true);
   	ctx.lineTo(px,px);
     ctx.closePath();
   	ctx.fill();
-    ctx.fillStyle = '#D2DDF0';
+    ctx.fillStyle = '#26B4D5';
     ctx.beginPath();
     ctx.moveTo(px,px);
-    ctx.arc(px,px,px,slice, 0,false);
+    ctx.arc(px,px,px,slice, 0,true);
     ctx.lineTo(px,px);
     ctx.closePath();
   	ctx.fill();
@@ -210,7 +286,7 @@ function drawPie (total) {
 
   function animate () {
     percent+= total/40;
-    if (percent>=total) afterDraw(pie, total);
+    if (percent>total) afterDraw(pie, total);
     else chart();
   }
 
